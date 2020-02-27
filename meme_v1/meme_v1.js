29b0e25c72fc8fa2a147f8beaa0e3ce45cf3bbd2
Aaron Garcia <Aaron.garcia2017@gmail.com>
2020-02-27 17:09:26 -0500

    Meme Draft 1

--- meme_v1/meme_v1.js
+++ meme_v1/meme_v1.js
@@ -1,21 +1,32 @@
+//global scope
+	var vibecheck;
+	function preload(){
+		vibecheck = loadImage("vibecheck.jpg")
+
+	}
 function setup() {
 	createCanvas(500,500);
 
 background('gray');
+	image(vibecheck, 0, 0);
+
 
 	textSize(100);
 	fill('white');
 	stroke('black');
 	strokeWeight(5);
-	textStyle(ITALIC);
+	textStyle(BOLD , ITALIC);
 	textAlign(LEFT, CENTER);
 	text('THIS WAY',0, 50);
 textSize(60);
 textAlign(CENTER);
+
 	text('FOR YOUR VIBE CHECK, SIR.', 0, 280, 500, 300)
 
 	textSize(20)
 	strokeWeight(5);
 	textStyle(NORMAL);
 	textAlign(CENTER, TOP);
+
+
 }
