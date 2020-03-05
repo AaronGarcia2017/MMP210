8c71f19252e5b8eca3e7c6d05d55b610d91e9767
Aaron Garcia <Aaron.garcia2017@gmail.com>
2020-03-05 17:25:45 -0500

    Meme 2 Finished!

--- meme_v2/meme_v1.js
+++ meme_v2/meme_v1.js
@@ -1,27 +1,43 @@
 //global scope
-	var cat;
-	function preload(){
-		cat = loadImage("cat.jpg")
+var cat;
+function preload() {
+	cat = loadImage("cat.jpg")
+}
 
-
-
-	}
 function setup() {
 	createCanvas(300,190);
+}
 
-background('gray');
-	image(cat, 0, 0);
+function draw() {
 
+	background('gray');
+	// image(cat, 0, 0);
+
+	fill('white');
 	ellipse(20, 150, 30)
+	noStroke(0);
 
 
 	textSize(10);
-	fill('white');
-	textStyle(BOLD , ITALIC);
+	
+	textStyle(BOLD, ITALIC);
 	textAlign(LEFT, CENTER);
-	text('Put Mouse Here!',30, 130);
-textSize(60);
-textAlign(CENTER);
+	text('Put Mouse Here and Click!',30, 130);
+	textSize(60);
+	noStroke(0);
+	textAlign(CENTER);
 
+	if (mouseIsPressed) {
+		image(cat, 0, 0, width, height);
+		textSize(25);
+	fill('orange');
+	stroke('yellow');
+	strokeWeight(5);
+	textStyle(BOLD);
+	textAlign (LEFT, CENTER);
+	text('BONK!!',10, 110);
+	} else {
+		image(cat, 1000, 10, width, height);
+	}
 
 }
