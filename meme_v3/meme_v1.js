192801566c578c62d1b717b7c341271332eb47bf
Aaron Garcia <Aaron.garcia2017@gmail.com>
2020-03-10 17:31:26 -0400

    Dvd logo

---
+++ meme_v3/meme_v1.js
@@ -0,0 +1,36 @@
+//global scope
+var x = 100;
+var y = 100;
+var speed = 5;
+var c;
+var dvd;
+function preload() {
+	dvd = loadImage("dvd_logo.png")
+}
+function setup (){ 
+	createCanvas(400,400);	
+	c = 'blue';
+}
+
+function draw() {
+	background(0);
+
+	if (frameCount % 30 == 0) {
+		c = random(['Khaki', 'magenta', 'yellow', 'purple']);
+	}
+	  
+	tint(c);
+	frameRate(25);
+
+
+	image(dvd, x, y);
+	x += speed;
+	if (x > width || x < 0) {
+		speed *= -1;
+y += speed;
+	if (y > height || y < 0) {
+		speed *= -1;
+	}
+
+	}
+}
