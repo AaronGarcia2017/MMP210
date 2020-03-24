ad8dc35bf20c75c58718cdbb7d35caecbaa2057f
AaronGarcia2017 <aaron.garcia2017@gmail.com>
2020-03-24 16:12:40 -0400

    Meme 3 finished!

--- meme_v3/meme_v1.js
+++ meme_v3/meme_v1.js
@@ -5,32 +5,65 @@ var speed = 5;
 var c;
 var dvd;
 function preload() {
-	dvd = loadImage("dvd_logo.png")
+  dvd = loadImage("dvd_logo.png");
 }
-function setup (){ 
-	createCanvas(400,400);	
-	c = 'blue';
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  x = random(width);
+  	y = random(height);
+
+  xspeed = 10;
+  yspeed = 10;
+ 	 pickColor();
+}
+
+function pickColor() {
+  r = random(100, 256);
+  g = random(100, 256);
+  b = random(100, 256);
+
 }
 
 function draw() {
-	background(0);
+  background(0);
+  tint(r, g, b);
 
-	if (frameCount % 30 == 0) {
-		c = random(['Khaki', 'magenta', 'yellow', 'purple']);
-	}
-	  
-	tint(c);
-	frameRate(60);
+  image(dvd, x, y);
 
-//Dvd logo
-	image(dvd, x, y);
-	x += speed;
-	if (x > width || x < 1) {
-		speed *= -1;
-y += speed;
-	if (y > height || y < 0) {
-		speed *= -5;
-	}
 
-	}
+  frameRate(25);
+
+
+  x = x + xspeed;
+  y = y + yspeed;
+
+  if (x + dvd.width >= width) {
+    xspeed = -xspeed;
+
+    x = width - dvd.width;
+    pickColor();
+  } else if (x <= 0) 
+  {
+    xspeed = -xspeed;
+    x = 0;
+
+    pickColor();
+  }
+
+  if (y + dvd.height >= height) {
+    yspeed = -yspeed;
+
+    y = height - dvd.height;
+    pickColor();
+
+  }
+
+   else if (y <= 0) {
+    yspeed = -yspeed;
+    y = 0;
+
+    pickColor();
+  }
 }
