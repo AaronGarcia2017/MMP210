efdeec3552f1e40b18d42ca2cd4533e0e36a472f
AaronGarcia2017 <aaron.garcia2017@gmail.com>
2020-05-20 22:19:09 -0400

    Final Project Submission

--- button_game/meme_v1.js
+++ button_game/meme_v1.js
@@ -3,10 +3,15 @@ var x, y;
 var score = 0;
 var time = 0;
 var endtime = 600;
+var music;
+var endmusic;
+var gameOver = false; 
 function preload() {
   music = loadSound('ding.mp3');
+  endmusic = loadSound('buzzer.wav');
   }
 function update() {
+  music.play();
  r = random(0, 255);
     g = random(0, 255);
     b = random(0, 255);
@@ -27,7 +32,11 @@ ellipse(x,y,50);
 if (time>=endtime) {
   textSize(30);
   text("Score: "+ score, 10,100);
-   text("Reload the Page to try again!", 10, 60);
+   text("Press the ENTER key to try again!", 10, 60); 
+   if (!gameOver) {
+  endmusic.play();
+}
+gameOver = true;
 } else{
 time ++;
 }
@@ -35,16 +44,29 @@ textSize(20);
  text("Time: "+ time+"/"+endtime, 10,30);
 }
 
+function reset() {
+  gameOver = false;
+  time = 0;
+  score = 0;
+  update();
+}
+
+function keyPressed() {
+  if (keyCode == 13) { // Enter key
+    reset();
+  }
+}
 
 function mousePressed() {
-    var d = dist(mouseX, mouseY, x, y);
-  if (d < 25 && time<endtime) {
-   update();
-   score ++;
-
-
+  var d = dist(mouseX, mouseY, x, y);
+  if (d < 25) {
+     if (time < endtime) {
+      update();
+      score ++;
+    } else {
+      reset();
+    }    
   }
-
-
-
 }
+
+
